44cef45ae2c322a81138fe0932a2b7dcf5d8edec
Roberto <>
2018-03-28 23:13:27 +0200

    plato to sg

--- src/brew_data/analysis.clj
+++ src/brew_data/analysis.clj
@@ -51,10 +51,74 @@
                                           (map second)
                                           set) (get r 4)))))))))
 
-(comment
+(defn map-csv [from to map-fn]
+  (with-open [reader (io/reader from)
+              writer (io/writer to)]
+    (->> (csv/read-csv reader)
+         (map #(map-fn %))
+         (csv/write-csv writer))))
 
-  (get (headers) 4)
 
-  (count (take-sample 1000))
+(defn col-frequencies 
+  "Apply func frequencies to a column of a CSV"
+  [col-idx]
+  ((with-csv recipe-file) 
+   (fn [rows] (->> rows
+                   rest
+                   (map #(get % col-idx))
+                   frequencies))))
 
+
+(defn rounder 
+  "Generate a function that round to a certain decimal"
+  [dec]
+  (let [mv (Math/pow 10 dec)]   
+    (fn [n] (-> n      
+                (* mv)
+                Math/round
+                (/ mv)))))
+
+(def round3 (rounder 3))
+
+(defn plato-to-sg [plato]
+  (as-> plato k
+    (/ k 258.2)
+    (* k 227.1)
+    (- 258.6 k)
+    (/ plato k)
+    (+ k 1)
+    (round3 k)))
+
+
+(defn row-to-sg [row]
+  (if (= "Plato" (get row 16))
+    (-> row
+        (update-in [7] #(-> % 
+                            read-string
+                            plato-to-sg))
+        (update-in [8] #(-> % 
+                            read-string
+                            plato-to-sg)))
+    row
+    )
+)
+
+(defn unifyOG-FG
+  ""
+  [col-idx]
+  ((with-csv recipe-file) 
+   (fn [rows] (->> rows
+                   rest
+                   (map #(get % col-idx))))))
+
+(comment 
+  ((with-csv recipe-file) 
+   (fn [rows] (->> rows
+                   rest
+                   (take 500)
+;;                   (map #(list (nth % 7) (nth % 8) (nth % 16)))
+                   (filter #(= "Plato" (nth % 16)))
+;                   (map row-to-sg)
+                   doall)))
+  
   )
